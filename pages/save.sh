f0fbdd6fa81bcb638e0b63a4d6739ac5d8163344
uncenter <47499684+uncenter@users.noreply.github.com>
2023-11-13 21:44:04 -0500

    fix: check if directory exists

--- pages/save.sh
+++ pages/save.sh
@@ -2,7 +2,7 @@ if [[ "$REQUEST_METHOD" != "POST" ]]; then
   return $(status_code 405)
 fi
 
-mkdir data/answers/
+[ ! -d "data/answers/" ] && mkdir data/answers/
 
 echo "${QUERY_PARAMS[answer]}" > "data/answers/${QUERY_PARAMS[question]}"
 
